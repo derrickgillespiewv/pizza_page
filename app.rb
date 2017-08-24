eea63e012ff7f7de44a9f47fd53e22508a588732
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-08-24 12:41:42 -0400

    CounterConfirm works, nil error unresolved

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
 require "sinatra"
-enable :session
+enable :sessions
 
 get '/' do
 erb :topping
 end 
 
+post '/all_tops' do 
+	session[:crust] = params[:crust]
+	session[:sauce] = params[:sauce]
+	session[:meat] = params[:meat]
+	session[:veggie] = params[:veggie]
+	session[:cheese] = params[:cheese]
+	p params[:sauce]
+	p params[:crust]
+	redirect '/confirm'
+end
+
+get '/confirm' do 
+	p params[:sauce]
+	p params[:crust]
+	erb :confirm, locals: {crust: session[:crust], sauce: session[:sauce], meat: session[:meat], veggie: session[:veggie], cheese: session[:cheese]}
+end 
+
+post '/confirm' do
+    session[:crust_choice] = params[:crust_radio] 
+    session[:sauce_choice] = params[:sauce_radio]
+    session[:meat_choice] = params[:meat_radio]
+    session[:veg_choice] = params[:veg_radio]
+    session[:cheese_choice] = params[:cheese_radio]
+    redirect '/results'
+end
